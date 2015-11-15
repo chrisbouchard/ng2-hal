7bd52c06bb3fda3909dcea12487395dde47123df
Chris Bouchard <chris@upliftinglemma.net>
2015-11-14 20:03:17 -0500

    Added Semantic UI to the project.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,10 @@
-import {bootstrap, Component} from 'angular2/angular2';
+import {bootstrap, Component, ViewEncapsulation} from 'angular2/angular2';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: '<h1>My First Angular 2 App</h1>',
+    encapsulation: ViewEncapsulation.Native,
+    styles: [ 'h1 { color: red; }' ]
 })
 class AppComponent { }
 
