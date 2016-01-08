f6b9d683b42510edcb4c2916fbe8130390c5623d
Chris Bouchard <chris@upliftinglemma.net>
2016-01-07 21:38:28 -0500

    Add skills to character sheet.

---
+++ src/pipes/indexed_pipe.ts
@@ -0,0 +1,17 @@
+import {Pipe, PipeTransform} from 'angular2/core';
+
+export interface Entry<E> {
+  index: number;
+  value: E;
+}
+
+@Pipe({ name: 'indexed', pure: true })
+export class IndexedPipe implements PipeTransform {
+
+  transform<E>(value: Iterable<E>): Array<Entry<E>> {
+    if (!value) return [];
+    return Array.from(value).map((value, index) => ({ index, value }));
+  }
+
+}
+
--- src/ui/sheet.ts
+++ src/ui/sheet.ts
@@ -4,12 +4,15 @@ import {RouterLink} from 'angular2/router';
 import {Character} from '../model/character';
 
 import {EntriesPipe} from '../pipes/entries_pipe';
+import {IndexedPipe} from '../pipes/indexed_pipe';
+import {ReversedPipe} from '../pipes/reversed_pipe';
 
 @Component({
   selector: 'fate-sheet'
 })
 @View({
-  pipes: [EntriesPipe],
+  pipes: [EntriesPipe, IndexedPipe, ReversedPipe],
+  styles: [require('./sheet.less')],
   template: require('./sheet.html.haml')
 })
 export class Sheet {
