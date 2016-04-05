1e4859fd75b598854064dc8ecd3a60330c8954bf
Chris Bouchard <chris@upliftinglemma.net>
2016-04-05 17:18:38 -0400

    Update more files.
    Continue to convert to HAL-only. #1

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,11 @@
 var path = require('path');
 var webpack = require('webpack');
 
-//var CommonsChunkPlugin = webpack.optimize.CommonsChunkPlugin;
 var DefinePlugin = webpack.DefinePlugin;
-var ProvidePlugin = webpack.ProvidePlugin;
 
 var DedupePlugin = webpack.optimize.DedupePlugin;
 var OccurenceOrderPlugin = webpack.optimize.OccurenceOrderPlugin;
 var UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
-
-//var ExtractTextPlugin = require('extract-text-webpack-plugin');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   verbose: true,
@@ -25,18 +20,7 @@ module.exports = {
   entry: {
     'app': [
       './src'
-    ] /*,
-    'vendor': [
-      'angular2/bundles/angular2-polyfills',
-      'angular2/common',
-      'angular2/core',
-      'angular2/http',
-      'angular2/platform/browser',
-      'angular2/router',
-      'babel-polyfill',
-      'jquery'
     ]
-    */
   },
 
   output: {
@@ -66,34 +50,6 @@ module.exports = {
         test: /\.ts$/,
         loader: 'babel!ts',
         exclude: [/node_modules/]
-      },
-      {
-        test: /\.css$/,
-        loader: 'css',
-        //include: [path.join(__dirname, 'src')]
-      },
-      /*
-      {
-        test: /\.css$/,
-        loader: ExtractTextPlugin.extract('style', 'css'),
-        exclude: [path.join(__dirname, 'src')]
-      },
-      */
-      {
-        test: /\.html.haml$/,
-        loader: 'haml-haml'
-      },
-      {
-        test: /\.html$/,
-        loader: 'html?attrs=false&minimize=false'
-      },
-      {
-        test: /\.less$/,
-        loader: 'raw!less'
-      },
-      {
-        test: /\.(eot|png|svg|ttf|woff|woff2)$/,
-        loader: 'url?limit=5000&name=assets/[name]-[hash].[ext]'
       }
     ],
     noParse: [
@@ -117,16 +73,9 @@ function devtool() {
 
 function commonPlugins() {
   return [
-    //new CommonsChunkPlugin('vendor', 'vendor.js'),
     new DefinePlugin({
       __PRODUCTION__: JSON.stringify(isProduction())
-    }),
-    //new ExtractTextPlugin('vendor.css'),
-    new HtmlWebpackPlugin({
-      inject: 'head',
-      template: './src/index.html.haml'
-    }),
-    new ProvidePlugin({ 'jQuery': 'jquery', '$': 'jquery' }),
+    })
   ];
 }
 
