69320b74d026aa3d01db0a65084869f53aeeca0f
Chris Bouchard <cbouchard@automate.com>
2016-08-19 17:00:18 -0400

    Refactor test, add cases.

--- lib/translators/default-object-translator.spec.ts
+++ lib/translators/default-object-translator.spec.ts
@@ -1,28 +1,37 @@
+import { Type } from '@angular/core';
+
 import { HalDefaultObjectTranslator } from './default-object-translator';
 
 describe('HalDefaultObjectTranslator', () => {
 
   const translator = new HalDefaultObjectTranslator();
 
-  it('applies to the undefined type', () => {
-    const applies = translator.appliesTo(undefined);
-    expect(applies).toBeTruthy();
-  });
+  const truthyTypes: any[] = [
+    { type: undefined, name: 'undefined' },
+    { type: null, name: 'null' }
+  ];
 
-  it('applies to the null type', () => {
-    const applies = translator.appliesTo(null);
-    expect(applies).toBeTruthy();
-  });
+  const falsyTypes: any[] = [
+    { type: Array, name: 'Array' },
+    { type: Boolean, name: 'Boolean' },
+    { type: Number, name: 'Number' },
+    { type: Object, name: 'Object' },
+    { type: String, name: 'String' }
+  ];
 
-  it('does not apply to the Object type', () => {
-    const applies = translator.appliesTo(Object);
-    expect(applies).toBeFalsy();
-  });
+  for (const entry of truthyTypes) {
+    it(`applies to the ${entry.name} type`, () => {
+      const applies = translator.appliesTo(entry.type);
+      expect(applies).toBeTruthy();
+    });
+  }
 
-  it('does not apply to the Array type', () => {
-    const applies = translator.appliesTo(Array);
-    expect(applies).toBeFalsy();
-  });
+  for (const entry of falsyTypes) {
+    it(`does not apply to the ${entry.name} type`, () => {
+      const applies = translator.appliesTo(entry.type);
+      expect(applies).toBeFalsy();
+    });
+  }
 
 });
 
