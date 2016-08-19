a281382c6f57411d70351380a0ab9d25e4e17db7
Chris Bouchard <cbouchard@automate.com>
2016-08-19 16:59:51 -0400

    Extend typings for Webpack's require.

--- custom_typings/webpack/require.d.ts
+++ custom_typings/webpack/require.d.ts
@@ -1,6 +1,15 @@
-declare var require: {
+declare interface RequireStatic {
   <T>(path: string): T;
   (paths: string[], callback: (...modules: any[]) => void): void;
-  ensure: (paths: string[], callback: (require: <T>(path: string) => T) => void) => void;
-};
+  ensure(paths: string[], callback: (require: <T>(path: string) => T) => void): void;
+  context(directory: string, useSubdirectories?: boolean, regExp?: RegExp): RequireContext;
+}
+
+declare interface RequireContext {
+  keys(): string[];
+  id: number;
+  <T>(path: string): T;
+}
+
+declare var require: RequireStatic;
 
