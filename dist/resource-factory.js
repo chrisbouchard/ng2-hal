97f189e85f895029c43398454522ff60b2ed2fa4
Chris Bouchard <chris@upliftinglemma.net>
2016-09-05 15:10:53 -0400

    Stop checking in build artifacts.

--- dist/resource-factory.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-class HalResourceFactory {
-}
-exports.HalResourceFactory = HalResourceFactory;
-//# sourceMappingURL=resource-factory.js.map
--- dist/translator.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-const core_1 = require('@angular/core');
-exports.HAL_OBJECT_TRANSLATORS = new core_1.OpaqueToken('halObjectTranslators');
-exports.HAL_COLLECTION_TRANSLATORS = new core_1.OpaqueToken('halCollectionTranslators');
-//# sourceMappingURL=translator.js.map
